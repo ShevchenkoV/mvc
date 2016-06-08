ee297c69fb05520e15bc655013a912f84daa53be
ShevchenkoV <elanceShevchenko@gmail.com>
2016-06-08 19:44:39 +0300

    init

---
+++ js/modules/controller.js
@@ -0,0 +1,51 @@
+define (
+    'modules/Controller',
+
+    ['jquery', 'lodash', 'modules/Model', 'modules/View'],
+
+    function () {
+
+        return function Controller (Model, View) {
+            console.log ('Controller is ready');
+            var self = this;
+
+            View.elements.addBtn.on('click', addItem);
+            View.elements.listContainer.on('click', '.item_delete', removeItem);
+            View.elements.listContainer.on('click', '.item_edit', editItem);
+
+            function addItem() {
+            	var newItem = View.elements.input.val();
+            	Model.addItem(newItem);
+            	View.renderList(Model.data);
+            	View.elements.input.val('');
+            }
+
+            function removeItem() {
+            	var item = $(this).attr('data-value');
+            	Model.removeItem(item);
+            	View.renderList(Model.data);
+            }
+
+            function editItem() {
+            	var index = $('.item_edit').index(this);
+            	console.log (index);
+                var item = $('li').eq(index).html();
+                var li = $('li').eq(index);
+                var input = '<input type="text" class="new" value="'+item+'"></input>';
+                li.after(input);
+                li.hide();
+                var btnSave = '<button class="item_save">Save</button>';
+                $(this).after(btnSave);
+                $('.item_save').on('click', saveItem);
+
+                function saveItem () {
+                   	item = $('.new').val();
+                	console.log (item);
+                	li.html(item).show();
+                	$('.new').remove();
+                    $('.item_save').remove();
+                };
+            }
+        }
+    }
+);
---
+++ js/modules/view.js
@@ -0,0 +1,33 @@
+define (
+    'modules/View',
+
+    ['jquery', 'lodash', 'modules/Model'],
+
+    function (Model) {
+        return function View(Model) {
+            console.log ('View is ready');
+            var self = this;
+
+            function init() {
+            	var wrapper =_.template($('#wrapper_tmpl').html());
+            	$('body').append(wrapper);
+
+                self.elements = {
+            		input         : $('.item_value'),
+            		addBtn        : $('.item_add'),
+            		listContainer : $('.item_list')
+            	};
+
+                self.renderList(Model.data);
+            };
+
+            self.renderList = function(data) {
+                var tmpl = _.template($('#list_tmpl').html());
+                var list = tmpl({data : data});
+                self.elements.listContainer.html(list);
+            };
+
+            init();
+        }
+    }
+);
